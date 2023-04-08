8dbd41b0c041fc11d3a404af05b08a921a8a2d6b
Prashanth-500apps <prashanth.uppada@500apps.com>
2023-04-08 16:30:31 +0530

    projects tasks

---
+++ components/collection/MainProjects.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div>
+      <CollectionListProject
+        :key="listRender"
+        :projectInfo="projectInfo"
+        @showAdd="showAdd"
+        @showEdit="showEdit"
+        @deleteProject="deleteProject"
+      />
+    </div>
+    <div v-if="show">
+      <CollectionAddProject @addProject="addProject" :key="renderAdd" />
+    </div>
+    <div v-if="showEditModal">
+      <CollectionEditProject
+        @editProject="editProject"
+        :updateData="updateData"
+        :key="renderEdit"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const show = ref(false)
+const renderAdd = ref(0)
+const renderEdit = ref(0)
+const showEditModal = ref(false)
+const listRender = ref(0)
+const updateData = ref({})
+// const url = ref('https://v1-orm-gharpe.mercury.infinity-api.net/api/projects/')
+
+const projectsData = useAuthLazyFetch(
+  `https://v1-orm-gharpe.mercury.infinity-api.net/api/projects/`,
+)
+
+const showAdd = (data: Boolean) => {
+  show.value = data
+  renderAdd.value++
+}
+
+let editIndex = ref('')
+
+const showEdit = (data, index) => {
+  showEditModal.value = data
+  editIndex.value = index
+  renderEdit.value++
+  updateData.value = data
+}
+
+let projectInfo = ref(projectsData.data._rawValue)
+
+const addProject = (body: Object) => {
+  const postOptions = {
+    body: body,
+  }
+  const addData = useAuthLazyFetchPost(
+    `https://v1-orm-gharpe.mercury.infinity-api.net/api/projects/`,
+    postOptions,
+  )
+  projectInfo.value.unshift(body)
+}
+
+const editProject = (body: Object) => {
+  const putOptions = {
+    body: body,
+  }
+  const editData = useAuthLazyFetchPut(
+    `https://v1-orm-gharpe.mercury.infinity-api.net/api/projects/${body.uid}`,
+    putOptions,
+  )
+  listRender.value++
+  // projectInfo.value.$set(editIndex.value,body)
+}
+
+const deleteProject = (data: object, index) => {
+  const deleteProjectData = useAuthLazyFetchDelete(
+    `https://v1-orm-gharpe.mercury.infinity-api.net/api/projects/${data.uid}`,
+  )
+  projectInfo.value.splice(index, 1)
+}
+</script>
